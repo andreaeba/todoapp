9ea5211a635688a4bff97ee5bb06968fe54d5706
Andrea Bermúdez <andreabusb@gmail.com>
2023-04-20 03:00:53 +0200

    local storage done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
 import { Box, Button, Card, CardBody, CardHeader, Flex, Heading, HStack, Stack, StackDivider, Text, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 import "./App.css";
-import { Input2 } from "./components/Input/Input";
+import { InputNewTask  } from "./components/Input/Input";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Tasks } from "./components/Tasks/Tasks";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       
       <div className="App">  
       <Navbar/>
-      
-      <Tasks/>
-          <HStack >
-            {[ ...Array(6)].map((item) => (
-                          <Card m="5px">
-                          <CardBody> 
-                            <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, perferendis?
-                            </Text>
-                          </CardBody>
-                        </Card>
-            ))}
-
-          </HStack>
+      <InputNewTask/>
         </div>
     </>
   );
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,36 +1,103 @@
-import { Container, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  Container,
+  FormControl,
+  FormHelperText,
+  HStack,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 
-export const Input2 = () => {
-  const [valueInputTask, setValueInputTaks] = useState("");
-  const handleChange = (e) => setValueInputTaks(e.target.value);
+export const InputNewTask = () => {
+  const [inputText, setInputText] = useState("");
+
+  const [categoryToDo, setCategoryToDo] = useState(
+    JSON.parse(localStorage.getItem("task-todo")) || []
+  );
+
+  const handleInputChange = (e) => {
+    const text = e.target.value;
+    // localStorage.setItem('task-todo', valueInputTask)
+    setInputText(text);
+    // console.log(inputText);
+  };
+
+  const handleSaveTask = (e) => {
+    e.preventDefault();
+    setCategoryToDo([...categoryToDo, inputText]);
+
+    localStorage.setItem(
+      "task-todo",
+      JSON.stringify([...categoryToDo, inputText])
+    );
+
+    // console.log(categoryToDo);
+    setInputText("");
+  };
+
+  console.log(JSON.parse(localStorage.getItem("task-todo")));
 
   return (
-    <Container>
-      <Text mb="8px">Value: {value}</Text>
-      <Input
-        value={value}
-        onChange={handleChange}
-        placeholder="Here is a sample placeholder"
-        size="sm"
-      />
-    </Container>
+    <>
+      <Box
+        // w='100vw'
+        // bg="red"
+        borderRadius="5px"
+        gap={2}
+      >
+        <FormControl
+          display="flex"
+          flexDirection="column"
+          // justifyContent="flex-start"
+          alignItems="flex-start"
+          marginTop="30px"
+        >
+          <Text mb="8px">What's the new task?</Text>
+          <Input
+            value={inputText}
+            onChange={handleInputChange}
+            placeholder="Add here your task!"
+            size="lg"
+          />
+
+          <Button colorScheme="blue" marginTop="10px" onClick={handleSaveTask}>
+            Add New Task
+          </Button>
+        </FormControl>
+      </Box>
+
+      <Box w="100vw" mt="20px" display="flex" gap={10}>
+        <Box w="30%" bg="gray.200" borderRadius="5px">
+          <Text textAlign="left" p={5}>
+            To do ⌛
+          </Text>
+          <Box>
+            {categoryToDo.map((task) => (
+              <Card key={task}
+              m="8px">
+                <CardBody>
+                  <Text>{task}</Text>
+                </CardBody>
+              </Card>
+            ))}
+          </Box>
+        </Box>
+
+        <Box w="25%" bg="gray.200" borderRadius="5px">
+          <Text textAlign="left" p={5}>
+            Doing 🤏
+          </Text>
+        </Box>
+        <Box w="25%" bg="gray.200" borderRadius="5px">
+          <Text textAlign="left" p={5}>
+            Done 😎
+          </Text>
+        </Box>
+      </Box>
+    </>
   );
 };
-
-// function Example() {
-//     const [value, setValue] = React.useState('')
-//     const handleChange = (event) => setValue(event.target.value)
-
-//     return (
-//       <>
-//         <Text mb='8px'>Value: {value}</Text>
-//         <Input
-//           value={value}
-//           onChange={handleChange}
-//           placeholder='Here is a sample placeholder'
-//           size='sm'
-//         />
-//       </>
-//     )
-//   }
