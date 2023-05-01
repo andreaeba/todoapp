2283000bf3f51dee1a0ee535424b86b86c5cc919
Andrea Bermúdez <andreabusb@gmail.com>
2023-05-01 21:02:15 +0200

    filters working

--- src/components/Input/InputNewTask.jsx
+++ src/components/Input/InputNewTask.jsx
@@ -4,8 +4,10 @@ import {
   Card,
   CardBody,
   FormControl,
+  FormLabel,
   Image,
   Input,
+  Select,
   Text,
   background,
   textDecoration,
@@ -14,7 +16,7 @@ import React, { useState } from "react";
 import plus from "../../assets/icons/plus.png";
 import edit from "../../assets/icons/edit.png";
 import check from "../../assets/icons/check.png";
-import delete2 from "../../assets/icons/delete.png"
+import delete2 from "../../assets/icons/delete.png";
 import { Todo } from "../Todo/Todo";
 
 export const InputNewTask = () => {
@@ -24,11 +26,13 @@ export const InputNewTask = () => {
     JSON.parse(localStorage.getItem(["tasks"])) || []
   );
 
+  const [tasks, setTasks] = useState([])
+
   let task = {
     id: Math.floor(Math.random() * 10000),
     text: inputText,
-    isComplete: false
-   }
+    isComplete: false,
+  };
 
   const handleInputChange = (e) => {
     const text = e.target.value;
@@ -39,13 +43,9 @@ export const InputNewTask = () => {
   const handleSaveTask = (e) => {
     e.preventDefault();
 
-    setToDo([ ...toDo, task ]);
+    setToDo([...toDo, task]);
 
-    localStorage.setItem(
-      "tasks",
-      JSON.stringify([ ...toDo, task ])
-    );
-
+    localStorage.setItem("tasks", JSON.stringify([...toDo, task]));
 
     setInputText("");
   };
@@ -53,54 +53,74 @@ export const InputNewTask = () => {
   // Eliminar Task
 
   const deleteTask = (id) => {
+    const isDelete = window.confirm(
+      `¿Está seguro que desea eliminar la tarea?`
+    );
 
-    const isDelete = window.confirm(`¿Está seguro que desea eliminar la tarea?`)
+    if (isDelete) {
+      const newListTaks = toDo.filter((task) => task.id !== id);
+      setToDo(newListTaks);
 
-    if(isDelete) {
-
-      const newListTaks = toDo.filter(task => task.id !== id)
-      setToDo(newListTaks)
-
-      localStorage.setItem(
-        "tasks",
-        JSON.stringify([toDo])
-      );
-
+      localStorage.setItem("tasks", JSON.stringify([toDo]));
     }
-  }
+  };
 
   // Editar un task
 
-  const editTask = (task) => {
- 
-
-  }
+  const editTask = (task) => {};
 
   // Hacer check a task
 
   const checkedTask = (id) => {
-
     let newArray = JSON.parse(localStorage.getItem("tasks")).map((task) => {
-      if(task.id == id) {
-
-        return { ...task, isComplete: !task.isComplete }
+      if (task.id == id) {
+        return { ...task, isComplete: !task.isComplete };
       }
-      return task
-    })
+      return task;
+    });
 
-    setToDo(newArray)
+    setToDo(newArray);
 
-    localStorage.setItem("tasks", JSON.stringify(newArray))
+    localStorage.setItem("tasks", JSON.stringify(newArray));
+  };
 
+  const filterTasks = (e) => {
+    console.log(e.target.value)
+
+    let tasksArray = JSON.parse(localStorage.getItem("tasks"))
+
+    if(e.target.value === "completed") {
+
+      const tasksCompleted = tasksArray.filter((task) => task.isComplete === true)
+      console.log(tasksCompleted)
+
+      setToDo(tasksCompleted)
+      return toDo
+
+    } 
+    
+    if (e.target.value === "pending") {
+
+      const tasksPending = tasksArray.filter((task) => task.isComplete === false)
+
+      setToDo(tasksPending)
+
+      return toDo
+
+    } 
+    
+    if (e.target.value === "all") {
+      setToDo(tasksArray)
+      return toDo
+
+    }
+   
   }
 
 
   return (
     <>
-      <Box
-        borderRadius="5px"
-        gap={2}
-      >
+      <Box borderRadius="5px" gap={2}>
         <FormControl
           display="flex"
           flexDirection="column"
@@ -123,33 +143,62 @@ export const InputNewTask = () => {
             gap={2}
           >
             <Image src={plus} width="14px" />
-            Add New Task 
+            Add New Task
           </Button>
         </FormControl>
       </Box>
 
-      <Box w="100vw" mt="20px" display="flex" gap={10} justifyContent="center" >
+      <Box marginTop="20px">
+        <FormControl onChange={filterTasks}>
+          <FormLabel>Filter tasks</FormLabel>
+          <Select placeholder="Select a filter">
+            <option value="pending">Pending</option>
+            <option value="completed">Completed</option>
+            <option value="all">All</option>
+          </Select>
+        </FormControl>
+      </Box>
+
+      <Box w="100vw" mt="20px" display="flex" gap={10} justifyContent="center">
         <Box w="35%" bg="gray.200" borderRadius="5px">
           <Text textAlign="left" p={5}>
             To do ⌛
           </Text>
           <Box>
             {toDo.map((task, id) => (
-              <Card key={id} m="8px" display="flex" flexDirection="row" alignItems="center" > 
+               <Card
+                key={id}
+                m="8px"
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                
+              >
                 <CardBody>
-                  <Text 
-                  className={toDo.filter(task => task.id == id) && `${task.isComplete ? 'checked' : ''}`}
+                  <Text
+                    className={
+                      toDo.filter((task) => task.id == id) &&
+                      `${task.isComplete ? "checked" : ""}`
+                    }
                   >
                     {task.text}
                   </Text>
-                  {/* <div className={selectedPosts.includes(post.id) && "myClass"} key={post.id}></div> */}
                 </CardBody>
-                <Box display="flex" flexDirection="row" gap={1} marginEnd="10px">              
-                  <Button bgColor="white"
-                  onClick={() => checkedTask(task.id)}
-                  ><Image src={check} width="14px"/></Button>
-                  <Button bgColor="white" onClick={() => editTask(task)} ><Image src={edit} width="14px" /></Button>
-                  <Button bgColor="white" onClick={() => deleteTask(task.id)}><Image src={delete2} width="14px" /></Button>
+                <Box
+                  display="flex"
+                  flexDirection="row"
+                  gap={1}
+                  marginEnd="10px"
+                >
+                  <Button bgColor="white" onClick={() => checkedTask(task.id)}>
+                    <Image src={check} width="14px" />
+                  </Button>
+                  <Button bgColor="white" onClick={() => editTask(task)}>
+                    <Image src={edit} width="14px" />
+                  </Button>
+                  <Button bgColor="white" onClick={() => deleteTask(task.id)}>
+                    <Image src={delete2} width="14px" />
+                  </Button>
                 </Box>
               </Card>
             ))}
@@ -159,4 +208,3 @@ export const InputNewTask = () => {
     </>
   );
 };
-
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,31 +19,6 @@ export const Navbar = () => {
           To do List
         </Heading>
       </header>
-
-      <nav>
-        <Box
-          bg="gray.200"
-          borderRadius="5px"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          marginTop="30px"
-        >
-          <List
-          w="100%"
-            display="flex"
-            gap={10}
-            p={3}
-            alignItems="left"
-            marginLeft="50px"
-          >
-            <ListItem>To do</ListItem>
-            <ListItem>Doing</ListItem>
-            <ListItem>Done</ListItem>
-            <ListItem>See All</ListItem>
-          </List>
-        </Box>
-      </nav>
     </>
   );
 };
